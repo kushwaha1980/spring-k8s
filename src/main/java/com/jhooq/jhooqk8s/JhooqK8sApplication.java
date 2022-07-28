898a194bf59f82f654e798f28a1447bdf1c87243
Deepak <ee_deepakkumar@rediffmail.com>
2022-07-28 18:03:06 +0530

    first comit

---
+++ src/main/java/com/jhooq/jhooqk8s/JhooqK8sApplication.java
@@ -0,0 +1,13 @@
+package com.jhooq.jhooqk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhooqK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhooqK8sApplication.class, args);
+	}
+
+}
